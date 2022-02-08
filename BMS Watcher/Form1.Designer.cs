c8b600ba2c2d735e607e157f52a81be0674ad182
Divins Mathew <divinsmathew@gmail.com>
2022-02-08 16:51:08 +0530

    Add project files.

---
+++ BMS Watcher/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace BMSWatcher
+{
+    partial class BMSWatcher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CountLabel = new System.Windows.Forms.Label();
+            this.LastCheckedLabel = new System.Windows.Forms.Label();
+            this.AttemptsLabel = new System.Windows.Forms.Label();
+            this.MovieID = new System.Windows.Forms.TextBox();
+            this.WatchButton = new System.Windows.Forms.Button();
+            this.WatchLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CountLabel
+            // 
+            this.CountLabel.AutoSize = true;
+            this.CountLabel.Location = new System.Drawing.Point(12, 47);
+            this.CountLabel.Name = "CountLabel";
+            this.CountLabel.Size = new System.Drawing.Size(99, 13);
+            this.CountLabel.TabIndex = 0;
+            this.CountLabel.Text = "Theatre Count : NA";
+            // 
+            // LastCheckedLabel
+            // 
+            this.LastCheckedLabel.AutoSize = true;
+            this.LastCheckedLabel.Location = new System.Drawing.Point(12, 70);
+            this.LastCheckedLabel.Name = "LastCheckedLabel";
+            this.LastCheckedLabel.Size = new System.Drawing.Size(111, 13);
+            this.LastCheckedLabel.TabIndex = 2;
+            this.LastCheckedLabel.Text = "Last Checked : Never";
+            // 
+            // AttemptsLabel
+            // 
+            this.AttemptsLabel.AutoSize = true;
+            this.AttemptsLabel.Location = new System.Drawing.Point(12, 95);
+            this.AttemptsLabel.Name = "AttemptsLabel";
+            this.AttemptsLabel.Size = new System.Drawing.Size(63, 13);
+            this.AttemptsLabel.TabIndex = 3;
+            this.AttemptsLabel.Text = "Attempts : 0";
+            // 
+            // MovieID
+            // 
+            this.MovieID.Location = new System.Drawing.Point(13, 13);
+            this.MovieID.Name = "MovieID";
+            this.MovieID.Size = new System.Drawing.Size(187, 20);
+            this.MovieID.TabIndex = 4;
+            // 
+            // WatchButton
+            // 
+            this.WatchButton.Location = new System.Drawing.Point(207, 11);
+            this.WatchButton.Name = "WatchButton";
+            this.WatchButton.Size = new System.Drawing.Size(66, 23);
+            this.WatchButton.TabIndex = 5;
+            this.WatchButton.Text = "Watch";
+            this.WatchButton.UseVisualStyleBackColor = true;
+            this.WatchButton.Click += new System.EventHandler(this.WatchButton_Click);
+            // 
+            // WatchLabel
+            // 
+            this.WatchLabel.Location = new System.Drawing.Point(3, 4);
+            this.WatchLabel.Name = "WatchLabel";
+            this.WatchLabel.Size = new System.Drawing.Size(277, 42);
+            this.WatchLabel.TabIndex = 6;
+            this.WatchLabel.Text = "Watching : ";
+            this.WatchLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.WatchLabel.Visible = false;
+            // 
+            // BMSWatcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 121);
+            this.Controls.Add(this.WatchButton);
+            this.Controls.Add(this.MovieID);
+            this.Controls.Add(this.AttemptsLabel);
+            this.Controls.Add(this.LastCheckedLabel);
+            this.Controls.Add(this.CountLabel);
+            this.Controls.Add(this.WatchLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "BMSWatcher";
+            this.ShowIcon = false;
+            this.Text = "BMS Watcher";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label CountLabel;
+        private System.Windows.Forms.Label LastCheckedLabel;
+        private System.Windows.Forms.Label AttemptsLabel;
+        private System.Windows.Forms.TextBox MovieID;
+        private System.Windows.Forms.Button WatchButton;
+        private System.Windows.Forms.Label WatchLabel;
+    }
+}
+
---
+++ BMS Watcher/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Media;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMSWatcher
+{
+    public partial class BMSWatcher : Form
+    {
+        int Attempts = 0;
+        long DataUsed = 0;
+        public BMSWatcher()
+        {
+            InitializeComponent();
+        }
+
+        void Crap()
+        {
+            int InitCount = -1, Count;
+
+            Task.Factory.StartNew(async delegate
+            {
+                while (true)
+                {
+                    using (WebClient client = new WebClient())
+                    {
+                        string Code = client.DownloadString("https://in.bookmyshow.com/buytickets/" + MovieID.Text);
+
+                        if (InitCount == -1)
+                            InitCount = Regex.Matches(Code, "<li class=\"list \"").Count;
+
+                        Count = Regex.Matches(Code, "<li class=\"list \"").Count;
+
+                        if (Count > InitCount)
+                            new SoundPlayer(Properties.Resources.Police).Play();
+
+                        Invoke(new Action(delegate
+                        {
+                            CountLabel.Text = "Theatre Count : " + Count.ToString();
+                            LastCheckedLabel.Text = "Last Checked : " + DateTime.Now.ToString();
+                            AttemptsLabel.Text = "Attempts : " + (++Attempts).ToString();
+                        }));
+                    }
+
+                    await Task.Delay(5000);
+                }
+            });
+        }
+
+        private async void WatchButton_Click(object sender, EventArgs e)
+        {
+            WatchButton.Enabled = false;
+            MovieID.Enabled = false;
+
+            if (!await Verify())
+            {
+                WatchButton.Enabled = true;
+                MovieID.Enabled = true;
+                return;
+            }
+
+            Crap();
+
+            WatchLabel.Visible = true;
+            WatchButton.Visible = false;
+            MovieID.Visible = false;
+        }
+        private async Task<bool> Verify()
+        {
+            if (string.IsNullOrWhiteSpace(MovieID.Text))
+                return false;
+
+            string Code = "";
+            try
+            {
+                await Task.Run(delegate
+                {
+                    Code = new WebClient().DownloadString("https://in.bookmyshow.com/buytickets/" + MovieID.Text);
+                });
+            }
+            catch
+            {
+                return false;
+            }
+
+            if (Code.IndexOf("cinema-name-wrapper") == -1)
+                return false;
+
+            for (int s = Code.IndexOf("\"name\" content=\"") + 16; Code[s + 2] != '/'; s++)
+                WatchLabel.Text += Code[s];
+
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebBrowser b = new WebBrowser();
+            b.ScriptErrorsSuppressed = true;
+            b.DocumentCompleted += B_Navigated;
+
+            b.Navigate("https://in.bookmyshow.com/kochi/movies/avengers-endgame/ET00090482");
+        }
+
+        private void B_Navigated(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            var b = (WebBrowser)sender;
+            DataUsed += b.DocumentText.Length;
+
+            if (b.Document.GetElementById("user-wts-true") != null)
+            {
+                CountLabel.Text = "Data Used : " + Clean(DataUsed);
+                LastCheckedLabel.Text = "Last Checked : " + DateTime.Now.ToString();
+                AttemptsLabel.Text = "Attempts : " + (++Attempts).ToString();
+
+                if (Attempts % 3 == 0 || Attempts % 2 == 0)
+                    b.Navigate("https://in.bookmyshow.com/kochi/movies/avengers-endgame/ET00090482");
+            }
+            else
+            {
+
+                new SoundPlayer(Properties.Resources.Police).Play();
+            }
+        }
+
+        private string Clean(long len)
+        {
+            string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" }; //Longs run out around EB
+            if (len == 0)
+                return "0" + suf[0];
+            long bytes = Math.Abs(len);
+            int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+            double num = Math.Round(bytes / Math.Pow(1024, place), 1);
+            return (Math.Sign(len) * num).ToString() + suf[place];
+        }
+    }
+}
